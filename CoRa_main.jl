fcf3abf7533be4f1231f8c05937d4167e0652caf
R0drigoA <rodrigoaguilar@ciencias.unam.mx>
2025-05-21 23:39:50 -0600

    modified:   CoRa_main.jl

--- CoRa_main.jl
+++ CoRa_main.jl
@@ -8,9 +8,9 @@ using DelimitedFiles;
 using Statistics
 
 # Main arguments
-iARG = (mm = "ATFv1_p02",		# Label for motif file
+iARG = (mm = "ATFv1_p02",       # Label for motif file
         ex = "Fig1",		# Label for parameters file
-        pp = :mY,			# Label for perturbation type
+        pp = :mY,		# Label for perturbation type
         ax = :mY,               # Label for condition/environment
         an = "Optimize"		# Label for analysis type (Explore, Optimize)
 )
@@ -19,35 +19,35 @@ iARG = (mm = "ATFv1_p02",		# Label for motif file
 fn = include(string("Library/FN_CoRa.jl"));
 
 # Perturbation details
-pert = (p   = iARG.pp,	# Parameter to be perturbed
+pert = (p   = iARG.pp,	        # Parameter to be perturbed
         d   = 1.05,		# Perturbation size (Delta rho)
-        c   = iARG.ax,	# Condition parameter
+        c   = iARG.ax,	        # Condition parameter
         r   = [-3.0,3.0],	# Range of conditions
-        eps = 0.1,
+        eps = 0.1,              # eps 
         coras = 50,             # Number of CoRa simulations
         solver = "fast",        # Solover to use ("fast", or "slow" (more precise))
-        tspan = (0.0, 1e8));        
+        tspan = (0.0, 1e8));    # tspan for slow solver    
 
 # Edit the analysis you want:
 
 # exploration details
 expl  = (n_params = 3,
-        pOp  = [:mU,:mW,:eP],	# Parameters to optimize
-        pMin = [-3.0, -2.0, -1.0],           # Mínimo log10 para cada uno
-        pMax = [3.0,  2.0,  1.0],            # Máximo log10 para cada uno      
-        n_points = 2048,           # number of points to evaluate
-        prtD =1);		# flag for printing full DY curve 
+        pOp  = [:mU,:mW,:eP],	        # Parameters to optimize
+        pMin = [-3.0, -2.0, -1.0],      # Min 
+        pMax = [3.0,  2.0,  1.0],       # Max     
+        n_points = 2048,                # number of points to evaluate
+        prtD =1);		        # flag for printing full curve 
 
 # Optimization details
 opt  = (n_params = 3,
-        pOp  = [:mU,:mW,:eP],	# Parameters to optimize
-        pMin = [-3.0, -3.0, -3.0],           # Mínimo log10 para cada uno
-        pMax = [3.0,  3.0,  3.0],            # Máximo log10 para cada uno      
-        iter = 10000,                            # number of points to evaluate
-        cov = [0.1, 0.1, 0.1],                  # covariance for each parameter
-        M = 10,                            # mutation step size
-        prtD =1,
-        rand = 1,
+        pOp  = [:mU,:mW,:eP],	        # Parameters to optimize
+        pMin = [-3.0, -3.0, -3.0],      # Mínimo log10 para cada uno
+        pMax = [3.0,  3.0,  3.0],       # Máximo log10 para cada uno      
+        iter = 10000,                   # number of points to evaluate
+        cov = [0.1, 0.1, 0.1],          # covariance for each parameter
+        M = 10,                         # mutation step size
+        prtD =1,                        # flag for printing full curve
+        rand = 1,                       # flag for random initial parameters
         );	
 
 
@@ -67,7 +67,7 @@ if (iARG.an == "Explore")
                 mm,         # model
                 p,          # core parameters
                 pert,       # perturbation details
-                expl,        # optimization details
+                expl,       # optimization details
                 u0)         # initial conditions
 elseif (iARG.an == "Optimize")
         fn.optimize(
